22c01789f2150c9d4620db99e4ac946476e2cf64
Simon <simonxd@gmail.com>
2016-06-23 21:00:33 -0300

    last mile ...

--- demo/demo.js
+++ demo/demo.js
@@ -6,84 +6,43 @@ const util = require('util');
 // third-party dependencies
 const fse      = require('fs-extra');
 const readdirp = require('readdirp');
+const Bluebird = require('bluebird');
 
-const Branch        = require('../lib/model/branch');
-const HappinessTree = require('../lib/ui');
+// the UITree Constructor
+const HappinessTree = require('../lib');
 
-var FS_ROOT_PATH = __dirname + '/files';
+// promisify some methods
+var _readdir = Bluebird.promisify(fs.readdir);
+var _lstat   = Bluebird.promisify(fs.lstat);
 
+// constants
+const FS_ROOT_PATH = __dirname + '/files';
 
+const hfs = {
+  readdirStats: function (p) {
+    // build the real path
+    p = path.join(FS_ROOT_PATH, p);
 
-// DIRECTORY MODEL
-function Dir(data) {
-  Branch.call(this, data);
-}
+    return _readdir(p)
+      .then((contents) => {
+        return Bluebird.all(contents.map((contentName) => {
 
-util.inherits(Dir, Branch);
+          var contentPath = path.join(p, contentName);
 
-Dir.prototype.BranchConstructor = Dir;
+          return _lstat(contentPath)
+            .then((stat) => {
 
-Dir.prototype.loadChildNodes = function () {
+              // process the stat object before returning
+              stat.basename = contentName;
 
-  var self         = this;
-  var absolutePath = this.absolutePath;
-
-  fs.readdir(absolutePath, function (err, contents) {
-
-    if (err) {
-      console.warn(err);
-    }
-
-    contents = contents || [];
-
-    contents.forEach(function (name) {
-      fs.lstat(absolutePath + '/' + name, function (err, stats) {
-
-        if (err) {
-          console.warn(err);
-        }
-
-        if (stats.isDirectory()) {
-          self.addBranch(name);
-        } else {
-          self.addLeaf(name);
-        }
+              return stat;
+            });
+        }));
       });
-    });
-  });
-}
-
-//////////////
-//////////////
-//////////////
-//////////////
-// UI STUFF //
-
-var treeModel = new Dir({
-  rootPath: FS_ROOT_PATH,
-});
-var treeUi = new HappinessTree('some-arbitrary-name');
-
-treeUi.attach(document.querySelector('body'));
-
-treeUi.addTreeEventListener('click', 'branch-label', function (data) {
-  console.log('click branch-label %s', data.path);
-  var branch = treeModel.getNode(data.path);
-  branch.loadChildNodes();
-});
-
-treeUi.addTreeEventListener('mouseover', 'leaf', function (data) {
-  console.log('mouseover leaf %s', data.path);
-});
-
-
-
-treeModel.on('node-added', function (parentNode, node, index) {
-  if (node.isBranch) {
-    treeUi.addBranch(parentNode.path, node.name, index);
-  } else {
-    treeUi.addLeaf(parentNode.path, node.name, index);
   }
-});
+};
 
-treeModel.loadChildNodes();
+// instantiate the ui
+var happiness = new HappinessTree('my-project', hfs);
+
+happiness.ui.attach(document.querySelector('body'));
