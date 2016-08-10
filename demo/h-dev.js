cc0c4e22c06d94d004f365157ffcc10367eba116
Simon <simonxd@gmail.com>
2016-08-10 16:53:06 -0300

    updated demo to work with h-fs directly

--- demo/h-dev.js
+++ demo/h-dev.js
@@ -1,9 +1,29 @@
-const createHDevElectron = require('h-dev-electron');
+const createHFs = require('h-fs');
+
+const Bluebird = require('bluebird');
 
 // constants
 const FS_ROOT_PATH = path.join(__dirname, '_demo_files');
 
-var hDevAPI = createHDevElectron(FS_ROOT_PATH);
+var hDevAPI = createHFs(FS_ROOT_PATH);
+
+hDevAPI.subscribe = function () {
+  console.log('subscribe', arguments);
+};
+
+hDevAPI.publish = function () {
+  console.log('publish', arguments);
+};
+
+hDevAPI.startWatching = function () {
+  console.log('startWatching', arguments);
+  return Bluebird.resolve();
+};
+
+hDevAPI.stopWatching = function () {
+  console.log('stopWatching', arguments);
+  return Bluebird.resolve();
+};
 
 // set implementation specific methods
 hDevAPI.projectRootURL = 'file://' + FS_ROOT_PATH;
